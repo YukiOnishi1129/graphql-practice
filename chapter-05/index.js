3c1158857396b86f189c1c4a0e008c848a863c94
yuki <yuki.onishi1129@gmail.com>
2021-04-30 22:30:53 +0900

    add: db accsess

--- chapter-05/index.js
+++ chapter-05/index.js
@@ -3,31 +3,58 @@ const express = require("express");
 const expressPlayground = require("graphql-playground-middleware-express")
   .default;
 const { readFileSync } = require("fs");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
+// スキーマを定義したテキストファイルを呼び出し
 const typeDefs = readFileSync("./typeDefs.graphql", "UTF-8");
+// リゾルバを呼び出し
 const resolvers = require("./resolvers");
 
-// express()を呼び出し、Expressアプリケーションを作成
-const app = express();
+// 非同期関数を作成
+async function start() {
+  // express()を呼び出し、Expressアプリケーションを作成
+  const app = express();
 
-// サーバーインスタンスを作成
-// その際、typeDefs(スキーマ)とリソルバを引数に取る
-const server = new ApolloServer({
-  typeDefs, // スキーマ
-  resolvers, // リゾルバ
-});
+  const MONGO_DB_HOST = process.env.DB_HOST;
 
-// applyMiddleware()を呼び出し、Expressにミドルウェアを追加する
-server.applyMiddleware({ app });
+  // DBアクセス
+  const client = await MongoClient.connect(MONGO_DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-// ホームルートを作成
-app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
-// playground用のルート
-app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+  const DB_NAME = process.env.DB_NAME;
 
-// 特定のポートでリッスンする
-app.listen({ port: 4000 }, () => {
-  console.log(
-    `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
-  );
-});
+  const db = client.db(DB_NAME); // DB名が必要
+  // console.log(client);
+
+  // DB接続をコンテキストオブジェクトに追加
+  const context = { db };
+
+  // サーバーインスタンスを作成
+  // その際、typeDefs(スキーマ)とリソルバを引数に取る
+  const server = new ApolloServer({
+    typeDefs, // スキーマ
+    resolvers, // リゾルバ
+    context, // コンテキスト
+  });
+
+  // applyMiddleware()を呼び出し、Expressにミドルウェアを追加する
+  server.applyMiddleware({ app });
+
+  // ホームルートを作成
+  app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
+  // playground用のルート
+  app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+  // 特定のポートでリッスンする
+  app.listen({ port: 4000 }, () => {
+    console.log(
+      `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
+    );
+  });
+}
+
+// start関数を実行 (DBにアクセスする)
+start();
--- chapter-05/resolvers/Query.js
+++ chapter-05/resolvers/Query.js
@@ -1,5 +1,14 @@
 module.exports = {
-  totalPhotos: () => photos.length,
+  totalPhotos: (parent, args, { db }) =>
+    db.collection("photos").estimatedDocumentCount(),
 
-  allPhotos: () => photos,
+  // estimatedDocumentCount(): コレクション内のカウントを取得
+
+  allPhotos: (parent, args, { db }) => db.collection("photos").find().toArray(),
+  // find().toArray(): 配列に変換
+
+  totalUsers: (parent, args, { db }) =>
+    db.collection("users").estimatedDocumentCount(),
+
+  allUsers: (parent, args, { db }) => db.collection("users").find().toArray(),
 };
