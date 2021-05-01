988ab49f3b844238b6c3ca6490118d7f8c9b8d5c
yuki <yuki.onishi1129@gmail.com>
2021-05-01 23:36:56 +0900

    add: github auth

---
+++ chapter-05/lib.js
@@ -0,0 +1,64 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+/**
+ * github tokenを取得するrequest
+ * @param {*} credentials
+ * @returns
+ *
+ * "Must specify access token via Authorization header. https://developer.github.com/changes/2020-02-10-deprecating-auth-through-query-param"
+ * 上記不具合の解決方法のヒント
+ * https://developer.mypurecloud.com/forum/t/client-authorization-example-using-node-fetch-instead-of-depreciated-request/10306/4
+ */
+const requestGithubToken = (credentials) => {
+  const client_id = credentials.client_id;
+  const client_secret = credentials.client_secret;
+  let auth =
+    "Basic " + Buffer.from(client_id + ":" + client_secret).toString("base64");
+  return fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: auth,
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ code: credentials.code }),
+  })
+    .then((res) => res.json())
+    .catch((error) => {
+      throw new Error(JSON.stringify(error));
+    });
+};
+
+/**
+ * github userアカウント情報を取得
+ * "Must specify access token via Authorization header. https://developer.github.com/changes/2020-02-10-deprecating-auth-through-query-param"
+ * https://developer.mypurecloud.com/forum/t/client-authorization-example-using-node-fetch-instead-of-depreciated-request/10306/4
+ * @param {*} token
+ * @returns
+ */
+const requestGithubUserAccount = (token) =>
+  fetch(`https://api.github.com/user`, {
+    method: "GET",
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => {
+      throw new Error(JSON.stringify(error));
+    });
+
+/**
+ * github認証情報取得
+ * @param {*} credentials
+ * @returns
+ */
+const authorizeWithGithub = async (credentials) => {
+  const { access_token } = await requestGithubToken(credentials);
+  const githubUser = await requestGithubUserAccount(access_token);
+
+  return { ...githubUser, access_token };
+};
+
+module.exports = { authorizeWithGithub };
--- chapter-05/resolvers/Mutation.js
+++ chapter-05/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const { authorizeWithGithub } = require("../lib");
+const fetch = require("node-fetch");
+const { ObjectID } = require("mongodb");
+
 // 1. ユニークIDをインクリメントするための変数
 let _id = 0;
 
@@ -15,5 +19,43 @@ module.exports = {
 
     // 3. 新しい写真を返す
     return newPhoto;
+  },
+
+  async githubAuth(parent, { code }, { db }) {
+    // 1. Githubからデータを取得する
+    let {
+      message,
+      access_token,
+      avatar_url,
+      login,
+      name,
+    } = await authorizeWithGithub({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code,
+    });
+
+    // 2. メッセージがある場合は何らかのエラーが発生している
+    if (message) {
+      throw new Error(message);
+    }
+
+    // 3. データを一つのオブジェクトにまとめる
+    let latestUserInfo = {
+      name,
+      githubLogin: login,
+      githubToken: access_token,
+      avatar: avatar_url,
+    };
+
+    // 4. 新しい情報を元にレコードを追加したり更新する
+    const {
+      ops: [user],
+    } = await db
+      .collection("users")
+      .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true });
+
+    // 5. ユーザーデータとトークンを返す
+    return { user, token: access_token };
   },
 };
