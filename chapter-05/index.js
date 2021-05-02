39e7a9c19ade904719a4c34323e2d1370e0e7f9b
yuki <yuki.onishi1129@gmail.com>
2021-05-02 11:18:04 +0900

    update: context

--- chapter-05/index.js
+++ chapter-05/index.js
@@ -27,17 +27,20 @@ async function start() {
   const DB_NAME = process.env.DB_NAME;
 
   const db = client.db(DB_NAME); // DB名が必要
-  // console.log(client);
 
   // DB接続をコンテキストオブジェクトに追加
-  const context = { db };
+  // const context = { db };
 
   // サーバーインスタンスを作成
   // その際、typeDefs(スキーマ)とリソルバを引数に取る
   const server = new ApolloServer({
     typeDefs, // スキーマ
     resolvers, // リゾルバ
-    context, // コンテキスト
+    context: async ({ req }) => {
+      const githubToken = req.headers.authorization;
+      const currentUser = await db.collection("users").findOne({ githubToken });
+      return { db, currentUser };
+    }, // コンテキスト (リクエストの度にユーザー情報をコンテキストに設定するため、関数の記述で使用)
   });
 
   // applyMiddleware()を呼び出し、Expressにミドルウェアを追加する
