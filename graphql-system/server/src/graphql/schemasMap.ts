61789ba16d68117ca60a6b9ce1f8f34f4a5855ce
yuki <yuki.onishi1129@gmail.com>
2021-05-07 18:06:59 +0900

    update: graphql typescript

---
+++ graphql-system/server/src/graphql/schemasMap.ts
@@ -0,0 +1,23 @@
+/**
+ * schema mapping
+ */
+import "graphql-import-node"; // アプリケーションに「.graphql」ファイルを取り組むライブラリ
+import { makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
+/* schemas */
+import * as userTypeDefs from "./schemas/user.graphql";
+import * as emptyTypeDefs from "./schemas/empty.graphql";
+/* resolvers */
+import resolvers from "./resolversMap";
+
+/**
+ * create schema
+ */
+const schema: GraphQLSchema = makeExecutableSchema({
+  // 先にQueryとMutationの型を定義して、他の全てのスキーマファイルを拡張するためにemptyを定義
+  // なので、先にemptyを記述しないとエラー
+  typeDefs: [emptyTypeDefs, userTypeDefs],
+  resolvers,
+});
+
+export default schema;
--- graphql-system/server/src/index.ts
+++ graphql-system/server/src/index.ts
@@ -1,16 +1,29 @@
 require("module-alias/register");
 // require("dotenv").config();
-import Express from "express";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+/* schema */
+import schema from "./graphql/schemasMap";
 
-const app = Express();
-const PORT = 4000;
+const app = express();
+const PORT = {
+  port: 4000,
+};
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello, World!");
+// });
+
+const server = new ApolloServer({
+  schema,
 });
+
+server.applyMiddleware({ app });
 
 app.listen(PORT, () => {
-  console.log("Server Started");
+  console.log(
+    `\n🚀    GraphQL is now running on http://localhost:${PORT}/graphql`
+  );
 });
 
-export default app;
+// export default app;
