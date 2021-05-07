467867c3c339fca92d605e9baa54e89b8750b47b
yuki <yuki.onishi1129@gmail.com>
2021-05-07 11:37:24 +0900

    add: docker-dev-enviroment

--- graphql-system/server/src/index.ts
+++ graphql-system/server/src/index.ts
@@ -3,7 +3,11 @@ require("module-alias/register");
 import Express from "express";
 
 const app = Express();
-const PORT = 3000;
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
 app.listen(PORT, () => {
   console.log("Server Started");
